c2517fdb7d568fead36332bdc40dc2bf1eb3e47a
zq <512641057@qq.com>
2018-11-12 10:38:49 +0800

    添加文章列表

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,22 +2,24 @@
 <div>
    <home-header></home-header>   
    <home-swiper></home-swiper>
-    <home-icons></home-icons>
+   <home-icons></home-icons>
+   <home-recommed></home-recommed>
 </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommed from './components/Recommed'
 
 export default {
     name:'Home',
     components:{
         HomeHeader,
         HomeSwiper,
-        HomeIcons
+        HomeIcons,
+        HomeRecommed
     }
-    
 }
 </script>
 <style>
---
+++ src/pages/home/components/Recommed.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="recommed border-bottom">
+        <div class="recommed-img">
+            <img src="http://img2.imgtn.bdimg.com/it/u=3445701240,1053655830&fm=15&gp=0.jpg" alt="">
+        </div>
+        <div class="recommed-context">
+              <div class="recommed-title">
+                   <p>我是超人</p>
+               </div>
+              <div class="recommed-text">
+                  <p>我是超人我是超人</p>
+               </div>
+        </div>
+      
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+
+    .recommed
+        display flex
+        width 100%
+        box-sizing border-box
+        line-height 0.4rem
+        padding .1rem
+        .recommed-img
+          width 2.5rem
+          >img 
+            width 2.5rem  
+        .recommed-context
+            flex 1
+            min-width 0
+            padding-left .1rem
+            .recommed-text
+                text-indent .2rem
+                ellipsis()
+</style>
+
